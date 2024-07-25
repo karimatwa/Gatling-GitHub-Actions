06f6d894678f7d85a58bdd0d3081271044ccdc9f
karimatwa <karim-atwa@hotmail.com>
2024-07-25 13:41:47 +0200

    gatling v3.11.5.2

--- src/gatling/java/computerdatabase/ComputerDatabaseSimulation.java
+++ src/gatling/java/computerdatabase/ComputerDatabaseSimulation.java
@@ -2,88 +2,91 @@ package computerdatabase;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
+
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
-import java.util.Properties;
+
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * This sample is based on our official tutorials:
+ * <ul>
+ *   <li><a href="https://gatling.io/docs/gatling/tutorials/quickstart">Gatling quickstart tutorial</a>
+ *   <li><a href="https://gatling.io/docs/gatling/tutorials/advanced">Gatling advanced tutorial</a>
+ * </ul>
+ */
 public class ComputerDatabaseSimulation extends Simulation {
-       
-        FeederBuilder<String> feeder = csv("search.csv").random();
-        Properties properties = new Properties();
-       
-        int usersCount = Integer.parseInt(System.getProperty("users", "6000"));
-        
-        @Override
-        public void before() {
-                System.out.printf("Running test with %d users%n", usersCount);
-        }
-        
-        ChainBuilder search = exec(http("Home").get("/"))
-                        .pause(1)
-                        .feed(feeder)
-                        .exec(
-                                http("Search")
-                                        .get("/computers?f=#{searchCriterion}")
-                                        .check(css("a:contains('#{searchComputerName}')", "href").saveAs("computerUrl")))
-                        .pause(1)
-                        .exec(
-                                http("Select")
-                                        .get("#{computerUrl}")
-                                        .check(status().is(200)))
-                        .pause(1);
 
-        ChainBuilder browse =
-                        // Note how we force the counter name, so we can reuse it
-                        repeat(4, "i").on(
-                                exec(
-                                        http("Page #{i}")
-                                        .get("/computers?p=#{i}"))
-                                        .pause(1));
+    FeederBuilder<String> feeder = csv("search.csv").random();
 
-        // Note we should be using a feeder here
-        // let's demonstrate how we can retry: let's make the request fail randomly and
-        // retry a given
-        // number of times
+    ChainBuilder search = exec(
+            http("Home").get("/"),
+            pause(1),
+            feed(feeder),
+            http("Search")
+                .get("/computers?f=#{searchCriterion}")
+                .check(
+                    css("a:contains('#{searchComputerName}')", "href").saveAs("computerUrl")
+                ),
+            pause(1),
+            http("Select")
+                .get("#{computerUrl}")
+                .check(status().is(200)),
+            pause(1)
+    );
 
-        ChainBuilder edit =
-                        // let's try at max 2 times
-                        tryMax(2)
-                                .on(
-                                        exec(
-                                                http("Form").get("/computers/new"))
-                                                .pause(1)
-                                                .exec(
-                                                http("Post")
-                                                .post("/computers")
-                                                .formParam("name",
-                                                                "Beautiful Computer")
-                                                .formParam("introduced",
-                                                                "2012-05-30")
-                                                .formParam("discontinued",
-                                                                "")
-                                                .formParam("company",
-                                                                "37")
-                                                .check(status().is(
-                                                // request
-                                                session -> 200 + ThreadLocalRandom.current().nextInt(2)))))
-
-                                        .exitHereIfFailed();
-
-        HttpProtocolBuilder httpProtocol = http.baseUrl("https://computer-database.gatling.io")
-                        .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-                        .acceptLanguageHeader("en-US,en;q=0.5")
-                        .acceptEncodingHeader("gzip, deflate")
-                        .userAgentHeader(
-                                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0");
-
-        ScenarioBuilder users = scenario("Users").exec(search
-        // , browse
+    // repeat is a loop resolved at RUNTIME
+    ChainBuilder browse =
+        // Note how we force the counter name, so we can reuse it
+        repeat(4, "i").on(
+            http("Page #{i}").get("/computers?p=#{i}"),
+            pause(1)
         );
-        ScenarioBuilder admins = scenario("Admins").exec(search, browse, edit);
-        Assertion assertion = global().responseTime().percentile(95.0).lt(300);
-        {
-                setUp(
-                                users.injectOpen(rampUsers(usersCount).during(35))).assertions(assertion).protocols(httpProtocol);
-        }
+
+    // Note we should be using a feeder here
+    // let's demonstrate how we can retry: let's make the request fail randomly and retry a given
+    // number of times
+
+    ChainBuilder edit =
+        // let's try at max 2 times
+        tryMax(2)
+            .on(
+                http("Form").get("/computers/new"),
+                pause(1),
+                http("Post")
+                    .post("/computers")
+                    .formParam("name", "Beautiful Computer")
+                    .formParam("introduced", "2012-05-30")
+                    .formParam("discontinued", "")
+                    .formParam("company", "37")
+                    .check(
+                        status().is(
+                            // we do a check on a condition that's been customized with
+                            // a lambda. It will be evaluated every time a user executes
+                            // the request
+                            session -> 200 + ThreadLocalRandom.current().nextInt(2)
+                        )
+                    )
+            )
+            // if the chain didn't finally succeed, have the user exit the whole scenario
+            .exitHereIfFailed();
+
+    HttpProtocolBuilder httpProtocol =
+        http.baseUrl("https://computer-database.gatling.io")
+            .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+            .acceptLanguageHeader("en-US,en;q=0.5")
+            .acceptEncodingHeader("gzip, deflate")
+            .userAgentHeader(
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/119.0"
+            );
+
+    ScenarioBuilder users = scenario("Users").exec(search, browse);
+    ScenarioBuilder admins = scenario("Admins").exec(search, browse, edit);
+
+    {
+        setUp(
+            users.injectOpen(rampUsers(10).during(10)),
+            admins.injectOpen(rampUsers(2).during(10))
+        ).protocols(httpProtocol);
+    }
 }
